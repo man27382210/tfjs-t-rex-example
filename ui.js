e152a215a340d829f74aefad5df5f34b1d21819a
twtseng <twtseng@indeed.com>
2023-06-19 17:08:10 +0900

    Init

---
+++ ui.js
@@ -0,0 +1,119 @@
+import './t-rex';
+import * as tf from '@tensorflow/tfjs';
+
+const CONTROLS = ['jump', 'noAction'];
+
+export function init() {
+  document.getElementById('controller').style.display = '';
+  statusElement.style.display = 'none';
+}
+
+function debounceLeading(func, timeout = 300){
+  let timer;
+  return (...args) => {
+    if (!timer) {
+      func.apply(this, args);
+    }
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      timer = undefined;
+    }, timeout);
+  };
+}
+
+const pressSpace = () => {
+  const spaceDown = new KeyboardEvent('keydown',{'keyCode':32,'which':32});
+  debounceLeading(document.dispatchEvent(spaceDown));
+}
+
+const trainStatusElement = document.getElementById('train-status');
+
+// Set hyper params from UI values.
+const learningRateElement = document.getElementById('learningRate');
+export const getLearningRate = () => +learningRateElement.value;
+
+const batchSizeFractionElement = document.getElementById('batchSizeFraction');
+export const getBatchSizeFraction = () => +batchSizeFractionElement.value;
+
+const epochsElement = document.getElementById('epochs');
+export const getEpochs = () => +epochsElement.value;
+
+const denseUnitsElement = document.getElementById('dense-units');
+export const getDenseUnits = () => +denseUnitsElement.value;
+const statusElement = document.getElementById('status');
+
+export function startTRex() {
+  pressSpace();
+}
+
+export function predictClass(classId) {
+  const action = CONTROLS[classId];
+  if (action === 'jump') {
+    pressSpace();
+  }
+}
+
+export function isPredicting() {
+  statusElement.style.visibility = 'visible';
+}
+export function donePredicting() {
+  statusElement.style.visibility = 'hidden';
+}
+export function trainStatus(status) {
+  trainStatusElement.innerText = status;
+}
+
+export let addExampleHandler;
+export function setExampleHandler(handler) {
+  addExampleHandler = handler;
+}
+let mouseDown = false;
+const totals = [0, 0, 0, 0];
+
+const jumpButton = document.getElementById('jump');
+const noActionButton = document.getElementById('noAction');
+
+const thumbDisplayed = {};
+
+async function handler(label) {
+  mouseDown = true;
+  const className = CONTROLS[label];
+  const button = document.getElementById(className);
+  const total = document.getElementById(className + '-total');
+  while (mouseDown) {
+    addExampleHandler(label);
+    document.body.setAttribute('data-active', CONTROLS[label]);
+    total.innerText = ++totals[label];
+    await tf.nextFrame();
+  }
+  document.body.removeAttribute('data-active');
+}
+
+jumpButton.addEventListener('mousedown', () => handler(0));
+jumpButton.addEventListener('mouseup', () => mouseDown = false);
+
+noActionButton.addEventListener('mousedown', () => handler(1));
+noActionButton.addEventListener('mouseup', () => mouseDown = false);
+
+
+export function drawThumb(img, label) {
+  if (thumbDisplayed[label] == null) {
+    const thumbCanvas = document.getElementById(CONTROLS[label] + '-thumb');
+    draw(img, thumbCanvas);
+  }
+}
+
+export function draw(image, canvas) {
+  const [width, height] = [224, 224];
+  const ctx = canvas.getContext('2d');
+  const imageData = new ImageData(width, height);
+  const data = image.dataSync();
+  for (let i = 0; i < height * width; ++i) {
+    const j = i * 4;
+    imageData.data[j + 0] = (data[i * 3 + 0] + 1) * 127;
+    imageData.data[j + 1] = (data[i * 3 + 1] + 1) * 127;
+    imageData.data[j + 2] = (data[i * 3 + 2] + 1) * 127;
+    imageData.data[j + 3] = 255;
+  }
+  ctx.putImageData(imageData, 0, 0);
+}
